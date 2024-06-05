9c32b9ae5270ba112d31d9c400258cf78012cfa3
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-06-05 08:03:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useState, createContext } from "react";
+import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
+
+// Buatlah sebuah context
+export const TodoContext = createContext();
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Progate React Course",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false,
+    },
+  ]);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      return;
+    }
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    // Bungkus app dengan provider dari context
+    <TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        <TodoForm addTodo={addTodo} />
+        <Todos todos={todos} />
+      </div>
+    </TodoContext.Provider>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "12px",
+  },
+  title: {
+    fontSize: "36px",
+  },
+};
+
+export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [title, setTitle] = useState("");
+
+  // Definisikan function handleSubmit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo(title);
+    setTitle("");
+  };
+
+  // Definisikan function "handleChangeTitle"
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // Periksa apakah function "handleChangeTitle" berfungsi
+  console.log(title);
+
+  return (
+    <div style={styles.container}>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Add your Todo"
+          style={styles.formInput}
+          // Tambahkan event handler "onChange"
+          onChange={(event) => {
+            handleChangeTitle(event);
+          }}
+          value={title}
+        />
+        <button style={styles.button}>Add Todo</button>
+      </form>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    marginBottom: "32px",
+  },
+  formInput: {
+    height: "66px",
+    width: "40%",
+    fontSize: "16px",
+    padding: "0 16px",
+  },
+  button: {
+    height: "72px",
+    fontSize: "16px",
+  },
+};
+
+export default TodoForm;
